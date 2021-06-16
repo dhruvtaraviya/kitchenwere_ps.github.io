4dcebfe852c84f638eb6f100469626c7e8de16ec
dhruvtaraviya <dhruvtaraviya2001@gmail.com>
2021-06-16 10:40:03 +0530

    kitchen

---
+++ js/resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('nav-list')
+rightNav = document.querySelector('.rightNav')
+
+
+
+burger.addEventListener('click', ()=>{
+    navRight.classlist.toggle(v-class-resp);
+    navList.classlist.toggle(v-class-resp);
+})
